58d2ab82073ada39ebaa4069179b234efade3f2c
Buhle Mwanza <buhle.mwanza@gmail.com>
2018-11-21 21:50:05 -0600

    curren menu and info pages

--- Info page/foodMenu.js
+++ Info page/foodMenu.js
@@ -1,25 +1,110 @@
 
+//---------------------------------------------------------------------------------
+//functions for increasing and decreasing the buttons
+//---------------------------------------------------------------------------------
+
 function increment() {
 
-    var i;
-    for(i = 0; i < 6; i++)
-    {
-        var n = document.getElementsByTagName("p")[0];
-        var idName;
-        if(n.id === "firstItem")
-        {
-            idName = "firstItem"
-           
-        }
-
-        else if(n.id === "secondItem")
-            idName = "secondItem";        
-    }
-
-    var n = document.getElementById(idName).innerHTML;
+    var n = document.getElementById("firstItem").innerHTML;
     var newNum = parseInt(n,10);
     newNum++;
-    document.getElementById(idName).innerHTML = newNum;
+    document.getElementById("firstItem").innerHTML = newNum;
+}
+
+function Decrease() {
+
+    var n = document.getElementById("firstItem").innerHTML;
+    var newNum = parseInt(n,10);
+    if(newNum > 0)
+    newNum--;
+    document.getElementById("firstItem").innerHTML = newNum;
+}
+
+
+function incrementFirst() {
+
+    var n = document.getElementById("secondItem").innerHTML;
+    var newNum = parseInt(n,10);
+    newNum++;
+    document.getElementById("secondItem").innerHTML = newNum;
+}
+
+function DecreaseFirst() {
+
+    var n = document.getElementById("secondItem").innerHTML;
+    var newNum = parseInt(n,10);
+    if(newNum > 0)
+    newNum--;
+    document.getElementById("secondItem").innerHTML = newNum;
+}
+
+function incrementSecond() {
+
+    var n = document.getElementById("thirdItem").innerHTML;
+    var newNum = parseInt(n,10);
+    newNum++;
+    document.getElementById("thirdItem").innerHTML = newNum;
+}
+
+function DecreaseSecond() {
+
+    var n = document.getElementById("thirdItem").innerHTML;
+    var newNum = parseInt(n,10);
+    if(newNum > 0)
+    newNum--;
+    document.getElementById("thirdItem").innerHTML = newNum;
+}
+
+function incrementThird() {
+
+    var n = document.getElementById("fourthItem").innerHTML;
+    var newNum = parseInt(n,10);
+    newNum++;
+    document.getElementById("fourthItem").innerHTML = newNum;
+}
+
+function DecreaseThird() {
+
+    var n = document.getElementById("fourthItem").innerHTML;
+    var newNum = parseInt(n,10);
+    if(newNum > 0)
+    newNum--;
+    document.getElementById("fourthItem").innerHTML = newNum;
+}
+  
+function incrementFourth() {
+
+    var n = document.getElementById("fifthItem").innerHTML;
+    var newNum = parseInt(n,10);
+    newNum++;
+    document.getElementById("fifthItem").innerHTML = newNum;
+}
+
+function DecreaseFourth() {
+
+    var n = document.getElementById("fifthItem").innerHTML;
+    var newNum = parseInt(n,10);
+    if(newNum > 0)
+    newNum--;
+    document.getElementById("fifthItem").innerHTML = newNum;
 }
     
-                        
+  
+function incrementFifth() {
+
+    var n = document.getElementById("sixItem").innerHTML;
+    var newNum = parseInt(n,10);
+    newNum++;
+    document.getElementById("sixItem").innerHTML = newNum;
+}
+
+function DecreaseFifth() {
+
+    var n = document.getElementById("sixItem").innerHTML;
+    var newNum = parseInt(n,10);
+    if(newNum > 0)
+    newNum--;
+    document.getElementById("sixItem").innerHTML = newNum;
+}
+    
+                                                
